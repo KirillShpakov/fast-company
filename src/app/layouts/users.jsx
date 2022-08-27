962c080140444e0fccb267aef33c27da11a74c52
Kirill Shpakov <krevetkalol4580@gmail.com>
2022-08-27 16:33:31 +0300

    added in api getById, install react-router-dom

--- src/app/layouts/users.jsx
+++ src/app/layouts/users.jsx
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { paginate } from "../utils/paginate";
-import Pagination from "./pagination";
+import Pagination from "../components/pagination";
 import api from "../api";
-import SearchStatus from "./searchStatus";
-import GroupList from "./groupList";
-import UserTable from "./usersTable";
+import SearchStatus from "../components/searchStatus";
+import GroupList from "../components/groupList";
+import UserTable from "../components/usersTable";
 import _ from "lodash";
 const Users = () => {
     const [currentPage, setCurrentPage] = useState(1);
