53472c392ac8f620fadf3f360a0bf1e24b592601
Kirill Shpakov <krevetkalol4580@gmail.com>
2022-08-29 16:42:27 +0300

    added user page

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 import Users from "./layouts/users";
+import Login from "./layouts/login";
+import Main from "./layouts/main";
 
 function App() {
     return (
-        <Users />
+        <div>
+            <Switch>
+                <Route path="/users/:userId?" component={Users} />
+                <Route path="/login" component={Login} />
+                <Route path="/" exact component={Main} />
+                <Redirect to="/" />
+            </Switch>
+        </div>
     );
 }
 
--- src/app/api/fake.api/user.api.js
+++ src/app/api/fake.api/user.api.js
@@ -149,7 +149,7 @@ const fetchAll = () =>
             resolve(users);
         }, 2000);
     });
-const getById = () =>
+const getById = (id) =>
     new Promise((resolve) => {
         window.setTimeout(function () {
             resolve(users.find((user) => user._id === id));
